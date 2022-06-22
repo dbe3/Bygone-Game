fd2e8331f95921f757ab9133856d1f4f1bf6d542
dbe3 <dcangel@student.apc.edu.ph>
2022-06-22 21:20:34 +0800

    Instructions

--- Assets/MainMenuScript.cs
+++ Assets/MainMenuScript.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenuScript : MonoBehaviour
 {
+    public GameObject Instructions;
+
     public void StartGame()
     {
         SceneManager.LoadScene("DemoLevel");
+    }
+
+    public void ShowInstructions()
+    {
+        Instructions.SetActive(true);
+    }
+
+    public void HideInstructions()
+    {
+        Instructions.SetActive(false);
     }
 
     public void QuitGame()
